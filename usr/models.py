01a165cd784b7a8aab34d02b23cb59017cdd231b
Anibladze1 <ako.nibladze1@gmail.com>
2022-12-19 17:20:14 +0400

    forth commit

--- usr/models.py
+++ usr/models.py
@@ -7,12 +7,9 @@ import datetime
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    birth_date = models.IntegerField(null=True)
+    birth_date = models.DateField(null=True)
 
     # def calculate_age(self):
     #     return int((datetime.datetime.now().year - int(self.birth_date) +1))
     #
     # age = property(calculate_age)
-
-    def __str__(self):
-        return self.name
